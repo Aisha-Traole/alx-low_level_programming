2e42224f936d91ec89ecda3d834fd2b9d3b6cfdb
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-22 01:35:07 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block 
+ * Description: Print alphabet lower case except q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z');
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+}
