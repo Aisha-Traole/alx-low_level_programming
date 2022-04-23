7f22d613aec2053504d96fe8ff34dd9f0955e98c
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-23 15:05:04 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - main block 
+ * Description : print alpabet lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
