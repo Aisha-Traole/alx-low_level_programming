7bb838667605b6486fe46a4a852b36fe33fb3b11
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-24 04:36:33 -0700

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - check for lower case
+ * Description: check caracter case
+ * Retur: 1 if true and 0 else
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
