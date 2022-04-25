3ab10a5b83e1b00093121f676069baee26c5aa02
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-25 02:27:57 -0700

    sm

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integer and return the result
+ * @a: first parameter
+ * @b: second parameter
+ * Description: add to numbers
+ * Return: Always (0)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
