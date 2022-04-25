9b9ae3a98df0a60c0d5ac09026c99d2637384cab
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-25 02:45:22 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,13 +22,13 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for(; n >= 98; n);
+		for(; n >= 98; n--)
 		{
-			printf("%d"; n);
+			printf("%d", n);
 			if (n == 98)
 				continue;
 			printf(",");
 		}
-		print("\n");
+		printf("\n");
 	}
 }
