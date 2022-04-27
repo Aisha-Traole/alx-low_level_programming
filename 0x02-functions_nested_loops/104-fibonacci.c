8a6d0874b2cfc758e15f7fa414b434563d1d88c0
Aisha-Traole <aicha.traore@inphb.ci>
2022-04-27 04:43:16 -0700

    4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2,k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
@@ -20,15 +20,15 @@ int main(void)
 		j = k - j;
 	}
 
-	j1 = j / 100000000;
-	j2 = j % 100000000;
-	k1 = k / 100000000;
-	k2 = k % 100000000;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 100000000));
-		printf("%lu", k2 % 100000000);
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
